590dc05bf03a8a02050364f77e3a77760a7b33d7
frednboria <f.lang@hotmail.fr>
2015-05-04 18:06:08 +0200

    correction tests et script resistance bruit

--- resistance_rotate_1i.sh
+++ resistance_rotate_1i.sh
@@ -5,7 +5,7 @@ echo "resultat image $1-$2.pgm" >> resultat/rotate/$1-$2_rotate_ind$3.dat
 for angle in `seq 1 20`;do
 ANGLE=`echo "$angle *0.15" | bc`
 	echo ANGLE=$ANGLE
-	./build/imgRotate -i database/$1/$1-$2.pgm -o tmp.pgm -n $ANGLE 2> /dev/null
+	./build/imgRotate -i database/$1/$1-$2.pgm -o tmp.pgm -a $ANGLE 2> /dev/null
 	res=$(./build/main -ind$3 tmp.pgm)
 	echo res=$res
 	echo $ANGLE $res>> resultat/rotate/$1-$2_rotate_ind$3.dat
