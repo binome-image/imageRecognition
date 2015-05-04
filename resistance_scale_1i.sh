1c12fda0a824893afc1b3cc4b0bb989ac60f7d11
frednboria <f.lang@hotmail.fr>
2015-05-04 17:25:27 +0200

    correction scripts resistance

--- resistance_scale_1i.sh
+++ resistance_scale_1i.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -f resultat/scale/$1_$2_scale.dat
-echo "resultat image $1-$2.pgm" >> resultat/scale/$1_$2_scale.dat
+rm -f resultat/scale/$1-$2_scale_ind$3.dat
+echo "resultat image $1-$2.pgm" >> resultat/scale/$1-$2_scale_ind$3.dat
 
 for scale in `seq 1 20`;do
 SCALE=`echo "$scale * 0.25" | bc`
@@ -8,5 +8,5 @@ SCALE=`echo "$scale * 0.25" | bc`
 	./build/imgScale -i database/$1/$1-$2.pgm -o tmp.pgm -s $SCALE 2> /dev/null
 	res=$(./build/main -ind$3 tmp.pgm)
 	echo res=$res
-	echo $SCALE $res>> resultat/scale/$1_$2_scale.dat
+	echo $SCALE $res>> resultat/scale/$1-$2_scale_ind$3.dat
 done
